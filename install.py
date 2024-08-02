3436af52235935f49217da0d5dd33b47178039a4
shiertier <90256303+shiertier@users.noreply.github.com>
2024-08-02 15:37:32 +0800

    Update install.py

--- install.py
+++ install.py
@@ -2,10 +2,10 @@ import sys
 import os.path
 import subprocess
 from huggingface_hub import snapshot_download
-
+from folder_paths import models_dir
 
 CUSTOM_NODES_PATH = os.path.dirname(os.path.abspath(__file__))
-WEIGHTS_PATH = os.path.join(CUSTOM_NODES_PATH, "models")
+WEIGHTS_PATH = os.path.join(models_dir, "models")
 HF_REPO_ID = "yisol/IDM-VTON"
 
 
