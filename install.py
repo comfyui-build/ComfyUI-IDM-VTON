b962fd8c6c9635b26e009842c69bfc4d08a044bd
shiertier <90256303+shiertier@users.noreply.github.com>
2024-08-02 15:49:16 +0800

    Update install.py

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ else:
     parent_dir = os.path.dirname(CUSTOM_NODES_PATH)
     models_dir = os.path.join(parent_dir, "models")
 
-WEIGHTS_PATH = os.path.join(models_dir, "models")
+WEIGHTS_PATH = os.path.join(models_dir, "IDM-VTON")
 
 def build_pip_install_cmds(args):
     if "python_embeded" in sys.executable or "python_embedded" in sys.executable:
@@ -26,4 +26,8 @@ def ensure_package():
 
 if __name__ == "__main__":
     ensure_package()
-    #snapshot_download(repo_id=HF_REPO_ID, local_dir=WEIGHTS_PATH, local_dir_use_symlinks=False)
+    if not os.path.exists(WEIGHTS_PATH) or not os.listdir(WEIGHTS_PATH):
+        snapshot_download(repo_id=HF_REPO_ID, local_dir=WEIGHTS_PATH, local_dir_use_symlinks=False)
+    else:
+        print(f"{WEIGHTS_PATH} already exists and is not empty. No need to download.")
+        print(f"If a model loading error occurs, please delete the directory: {WEIGHTS_PATH}.")
